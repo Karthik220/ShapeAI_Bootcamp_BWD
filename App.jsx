04c8de18f68565e914fc6596116cd1690076c374
Karthik220 <86551849+Karthik220@users.noreply.github.com>
2021-06-27 15:34:16 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Heading from "./Heading";
+import Footer from "./Footer";
+import Info from "./Info";
+
+
+function App(){
+  return (
+    <div>
+      <Heading />
+      <Footer />
+      <Info />
+      <Info />
+      <Info />
+      <Info />
+      <Info />
+      </div>
+  );
+}
+
+export default App
---
+++ Heading.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Heading(){
+  return (
+    <div>
+      <header>
+        <h1>ShapeAI Bootcamps</h1>
+      </header>
+      </div>
+  );
+}
+
+export default Heading
